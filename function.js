c96a5c4f563089565f46e410f352c2a326f1e9be
Aldi Mahardiansyah <41710133+aldimahardiansyah@users.noreply.github.com>
2022-04-03 05:26:20 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,34 @@
+// function luas_persegi(sisi){
+//     hasil = sisi * sisi;
+//     console.log(hasil);
+// }
+
+// luas_persegi(10);
+
+// const time = 09.03
+// if (time < 10) {
+//   greeting = "Good morning";
+// } else if (time < 20) {
+//   greeting = "Good day";
+// } else {
+//   greeting = "Good evening";
+// }
+
+// console.log(greeting);
+
+// const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+
+// let text = "";
+// for (let i = 0; i < cars.length; i++) {
+//   text += cars[i] + "<br>";
+// }
+
+for(let i=0; i<10; i++){
+    console.log("Perulangan ke-" + i);
+}
+
+let i = 0;
+while(i<10){
+    console.log("Perulangan ke-" + i);
+    i++;
+}
